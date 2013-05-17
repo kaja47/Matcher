70087843d3ee924323c3947a8942d2fc92911f99
K <kaja47@k47.cz>
2013-05-17 22:51:24 +0200

    Almost complete rewrite. Matcher uses SimpleXML instead of DOM, everything else is much better and much more consistent.

--- Matcher.php
+++ Matcher.php
@@ -13,37 +13,126 @@ namespace Atrox;
 
 class Matcher {
 
-  private $f;
+  const toString  = '\Atrox\Matcher::toString';
+  const oneline   = '\Atrox\Matcher::oneline';
+  const normalize = '\Atrox\Matcher::normalize';
+  const identity  = '\Atrox\Matcher::identity';
 
-  function __construct($f) { $this->f = $f; }
+  private $f;
+  private $m; // path
+
+  function __construct($f, $m = null) { $this->f = $f; $this->m = $m; }
   function __invoke()      { return call_user_func_array($this->f, func_get_args()); }
 
-  function processHtml($html) { $dom = new \DOMDocument(); @$dom->loadHTML($html); return $this($dom); }
-  function processXml ($xml)  { $dom = new \DOMDocument(); $dom->loadXML($xml);    return $this($dom); }
-  function processDom ($dom)  { return $this($dom); }
 
-
-  function fromHtml() { $self = $this; return function($html) use($self) { return $self->processHtml($html); }; }
-  function fromXml()  { $self = $this; return function($html) use($self) { return $self->processXml($html); }; }
-
-
-  /** @internal */
-  static function defaultExtractor($n) { return $n instanceof \DOMNode ? $n->nodeValue : $n; }
-  static function normalizeWhitespaces($n) { return trim(preg_replace('~\s+~', ' ', $n->nodeValue)); }
-  //static function defaultExtractor($ns) { $res = ''; foreach ($ns as $n) { $res .= $n->nodeValue; } return $res; }
-
-
-  /** Applies function $f to result of matcher (*after* extractor) */
-  function andThen($f) { // todo
-    $self = $this;
-    return new Matcher(function ($dom, $contextNode = null, $extractor = null) use($self, $f) {
-      return $f($self($dom, $contextNode, $extractor));
-    });
+  /**
+   *  @param string|array|int|Closure|Matcher $path
+   */
+  static function multi($path) {
+    $args = func_get_args();
+    $path = array_shift($args);
+    $m = new Matcher(function($node, $extractor = null) use($path) {
+      $extractor = Matcher::_getExtractor($extractor);
+      if (is_string($path)) return array_map($extractor, $node->xpath($path));
+      else                  return Matcher::_evalPath($node, $path, $extractor);
+    }, $path);
+    return empty($args) ? $m : $m->raw()->_deepMapNode(call_user_func_array('\Atrox\Matcher::multi', $args));
   }
 
 
-  /** regexes without named patterns will return numeric array without key 0
-   *  if result of previous matcher is array, it recursively applies regex on every element of that array
+  /**
+   *  @param string|array|int|Closure|Matcher $path
+   */
+  static function single($path) {
+    $args = func_get_args();
+    $path = array_shift($args);
+    $m = new Matcher(function ($node, $extractor = null) use($path) {
+      $extractor = Matcher::_getExtractor($extractor);
+      return Matcher::_evalPath($node, $path, $extractor);
+    }, $path);
+    return empty($args) ? $m : $m->raw()->_deepMapNode(call_user_func_array('\Atrox\Matcher::single', $args));
+  }
+
+
+  static function loadHTML($html) {
+    $dom = new \DOMDocument();
+    @$dom->loadHTML($html);
+    return simplexml_import_dom($dom);
+  }
+
+
+  function fromHtml($ex = null) { $m = new Matcher(function($x) { return Matcher::loadHTML($x); }); return $m->map($this->withExtractor($ex)); }
+  function fromXml($ex = null)  { $m = new Matcher('simplexml_load_string');                        return $m->map($this->withExtractor($ex)); }
+
+
+  // extractors:
+
+  private static function nodeToString($n) { return dom_import_simplexml($n)->nodeValue; }
+  static function toString($n)  { return trim(preg_replace('~ +~', ' ', self::nodeToString($n))); }
+  static function oneline($n)   { return trim(preg_replace('~\s+~', ' ', self::nodeToString($n))); }
+  static function normalize($n) { return trim(preg_replace('~[ \t]+~', " ", preg_replace('~\n{3,}~', "\n\n", preg_replace('~([ \t]+$|^[ \t]+)~m', '', self::nodeToString($n))))); }
+  static function identity($n)  { return $n; }
+  static function distr($f)     { return function ($x) use ($f) { return array_map($f, $x); }; }
+
+
+  /** @param callback $f  SimpleXMLElement => ? */
+  function withExtractor($f) {
+    $self = $this->f;
+    return new Matcher(function ($node, $extractor = null) use($self, $f) { // outer extractor passed as argument is thrown away
+      return $self($node, $f);
+    }, $this);
+  }
+
+  function raw() { return $this->withExtractor(Matcher::identity); }
+
+
+  /** maps only SimpleXMLElement in arbitrary depth */
+  private function _deepMapNode(Matcher $f) {
+    $mapf = function ($node, $extractor) use($f, &$mapf) {
+
+      if ($node instanceof \SimpleXMLElement) return call_user_func($f, $node, $extractor);
+      if (is_object($node))                   return (object) $mapf(get_object_vars($node), $extractor);
+      if (is_array($node))                    return empty($node) ? array() : array_combine(array_keys($node), array_map($mapf, $node, array_fill(0, count($node), $extractor)));
+      else                                    return $node;
+    };
+
+    return $this->mapEx($mapf);
+  }
+
+
+  /** Applies function $f to result of matcher (*after* extractor) */
+  function map($f) { return $this->mapEx(function ($node, $extractor) use ($f) { return call_user_func($f, $node); }); }
+  function andThen($f) { return $this->map($f); }
+
+
+  private function mapEx($f) {
+    $self = $this->f;
+    return new Matcher(function ($node, $extractor = null) use($self, $f) {
+      return $f($self($node, $extractor), $extractor);
+    }, $this);
+  }
+
+
+  /**
+   * monadic bind function (may not work)
+   * @param callback $f: A => Matcher[B]
+   */
+  function flatMap($f) {
+    $self = $this->f;
+    return new Matcher(function ($node, $extractor = null) use($self, $f) {
+      $m = $f($self($node, Matcher::identity));
+      return $m($node, $extractor);
+    }, $this);
+  }
+
+
+  function asInt()   { return $this->map('intval'); }
+  function asFloat() { return $this->map('floatval'); }
+
+
+  /**
+   * regexes without named patterns will return numeric array without key 0
+   * if result of previous matcher is array, it recursively applies regex on every element of that array
    */
   function regex($regex) {
     $f = function ($res) use($regex, & $f) { // &$f for anonymous recursion
@@ -68,85 +157,91 @@ class Matcher {
         throw new \Exception("Method `regex' should be applied only to Matcher::single which returns string or array of strings");
       }
     };
-    return $this->andThen($f);
+    return $this->map($f);
   }
 
 
-  /** defaultExtractor == null => use outer extractor
-   *  @param string $basePath
-   *  @param array|object $paths
-   *  @param callable|null $defaultExtractor
-   */
-  static function multi($basePath, $paths = null, $defaultExtractor = null) {
-    return new Matcher(function($dom, $contextNode = null, $extractor = null) use($basePath, $paths, $defaultExtractor) {
-      $xpath = new \DOMXpath($dom);
-      $extractor = Matcher::_getExtractor($defaultExtractor, $extractor);
-      $matches = $xpath->query($basePath, $contextNode);
+  /** experimental and uncomprehensible */
+  function seqOr(Matcher $that) {
+    $self = $this;
+
+    $thisPath = $this;
+    while ($thisPath instanceof Matcher) {
+      $thisRaw  = $thisPath->raw();
+      $thisPath = $thisPath->m;
+    }
+    $thatPath = $that;
+    while ($thatPath instanceof Matcher) {
+      $thatRaw  = $thatPath->raw();
+      $thatPath = $thatPath->m;
+    }
+
+    if (!is_string($thisPath) || !is_string($thatPath)) throw new \Exception('Method seqOr can be used only with matchers matching against string.');
+
+    $mm = Matcher::multi("$thisPath | $thatPath")->raw();
+    return new Matcher(function ($node, $extractor = null) use ($mm, $self, $that, $thisRaw, $thatRaw) {
+      $rawByBoth = $mm($node, $extractor);
+
+      $run = function ($m) use ($node, $extractor) { $arr = $m($node, $extractor); return is_array($arr) ? $arr : array($arr); };
+
+      $raw     = array_merge($run($thisRaw), $run($thatRaw));
+      $matched = array_merge($run($self), $run($that));
 
       $return = array();
-
-      if (!$paths) {
-        foreach ($matches as $m) $return[] = call_user_func($extractor, $m);
-
-      } else {
-        foreach ($matches as $m) $return[] = Matcher::_extractPaths($dom, $m, $paths, $extractor);
+      foreach($rawByBoth as $n) {
+        if (($i = array_search($n, $raw, false)) !== false) {
+          $return[] = $matched[$i];
+        }
       }
-
       return $return;
-    });
+    }, $mm);
   }
 
 
-  static function single($path, $defaultExtractor = null) {
-    return new Matcher(function ($dom, $contextNode = null, $extractor = null) use($path, $defaultExtractor) {
-      $xpath = new \DOMXpath($dom);
-      $extractor = Matcher::_getExtractor($defaultExtractor, $extractor);
+  // actual runtime methods
 
-      if (is_array($path)) {
-        return Matcher::_extractPaths($dom, $contextNode, $path, $extractor);
-
-      } else {
-        return Matcher::_extractValue($extractor, $xpath->query($path, $contextNode));
-      }
-    });
+  /** @internal */
+  static function _getExtractor($extractor) {
+    return $extractor ?: Matcher::toString; // Use outer extractor passed as argument, if it's null, use default extractor
   }
 
 
   /** @internal */
-  static function _getExtractor($defaultExtractor, $extractor) {
-    if ($defaultExtractor !== null) return $defaultExtractor;
-    else if ($extractor === null)   return 'Atrox\Matcher::defaultExtractor'; // use default extractor
-    else                            return $extractor; // use outer extractor passed as explicit argument
+  static function _evalPath($node, $path, $extractor) { // todo
+    if ($path instanceof Matcher || $path instanceof \Closure) { // key => multipath
+      return $path($node, $extractor);
+
+    } elseif (is_array($path) || is_object($path)) { // key => array(paths)
+      return Matcher::_extractPaths($node, $path, $extractor);
+
+    } elseif (is_string($path)) { // key => path
+      $matches = $node->xpath($path);
+      return count($matches) === 0 ? null : call_user_func($extractor, $matches[0]);
+
+    } elseif (is_int($path)) { // key => position of child element
+      $ns = $node->children();
+      return call_user_func($extractor, $ns[$path]);
+
+    } else {
+      throw new \Exception("Invalid path. Expected string, int, array, stdClass object, Matcher object of function, ".gettype($val)." given");
+    }
   }
 
 
-  /** @internal */
-  static function _extractPaths($dom, $contextNode, $paths, $extractor) {
-    $xpath = new \DOMXpath($dom);
+  /** @internal **/
+  static function _extractPaths($node, $paths, $extractor) {
     $return = array();
 
     foreach ($paths as $key => $val) {
-      if (is_array($val)) { // path => array()
-        $n = $xpath->query($key, $contextNode)->item(0);
-        $r = ($n === null) ? array_fill_keys(array_keys($val), null) : self::_extractPaths($dom, $n, $val, $extractor);
-        $return = array_merge($return, $r); // todo: object result
-
-      } elseif ($val instanceof Matcher || $val instanceof \Closure) { // key => multipath
-        $return[$key] = $val($dom, $contextNode, $extractor);
-      
-      } elseif (is_string($val)) { // key => path
-        $return[$key] = self::_extractValue($extractor, $xpath->query($val, $contextNode));
+      if (is_int($key)) { // merge into current level
+        $return = array_merge($return, Matcher::_evalPath($node, $val, $extractor)); // extracted value shoud be array
 
       } else {
-        throw new \Exception("Invalid path. Expected string, array or marcher, ".gettype($val)." given");
+        $return[$key] = Matcher::_evalPath($node, $val, $extractor);
+
       }
     }
 
-    return $return;
-  }
-
-  /** @internal */
-  static function _extractValue($extractor, $matches) {
-    return $matches->length === 0 ? null : call_user_func($extractor, $matches->item(0));
+    return is_object($paths) ? (object) $return : $return;
   }
 }
